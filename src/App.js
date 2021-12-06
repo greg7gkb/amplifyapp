35ce5f85a58cbd2a45469bc28afc4b95668fd33e
Greg Benson <greg7gkb@gmail.com>
2021-12-06 15:56:40 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import React from 'react';
 import logo from './fam.jpg';
 import './App.css';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey guys, welcome to my channel!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created using React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Hey {user.username}, welcome to my channel, with auth!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Created using React
+            </a>
+            <p />
+            <button onClick={signOut}>Sign out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
